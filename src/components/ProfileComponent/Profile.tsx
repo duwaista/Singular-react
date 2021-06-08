9d7f37dc00084ba8f56b1448fb392d8cd0ed87c0
Vladislav Egorov <vlad.45.45@mail.ru>
2021-06-08 19:49:13 +0700

    User profile updated, add adaptive disign

--- src/components/ProfileComponent/Profile.tsx
+++ src/components/ProfileComponent/Profile.tsx
@@ -1,6 +1,7 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
+import "./Profile.css";
 import { AppState } from "../../store/store";
 import { IdType } from "../../types";
 import HeaderComponent from "../HeaderComponent/HeaderComponent";
@@ -16,11 +17,20 @@ export default function ProfileComponent(): JSX.Element {
 	return (
 		<>
 			<HeaderComponent title='Профиль' icon={true} />
-			<img
-				alt='User avatar'
-				src='https://sun9-6.userapi.com/impg/P-rSyyZDHttn6GAXDqtBYYLPY9crqOBzPk6tYw/pAUoGdRJLxo.jpg?size=256x256&quality=96&sign=79a38587d2c1a7b3bfa08954a07f5ec7&type=album'
-			/>
-			<span className='center'>{id}</span>
+			<div className='user-profile-info-container'>
+				<img
+					className='user-profile-avatar'
+					alt='User avatar'
+					src='https://sun9-6.userapi.com/impg/P-rSyyZDHttn6GAXDqtBYYLPY9crqOBzPk6tYw/pAUoGdRJLxo.jpg?size=256x256&quality=96&sign=79a38587d2c1a7b3bfa08954a07f5ec7&type=album'
+				/>
+				<div className='user-profile-info'>
+					<span className='user-profile-email'>{currentUser.email}</span>
+					<div className='user-edit-profile-button'>
+						<span>Редактировать</span>
+					</div>
+				</div>
+				<div className='profile-bottom-line'></div>
+			</div>
 		</>
 	);
 }
